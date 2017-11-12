d745d04feb2c707c56212537afe472409402f3c7
jianghongliang <876474290@qq.com>
2017-11-12 21:26:29 +0800

    create file 00

---
+++ Main.java
@@ -0,0 +1,32 @@
+/**   
+ * @Title: Main.java
+ * @Package 
+ * @Description: TODO(用一句话描述该文件做什么)
+ * @author 江洪亮
+ * @date 2017年11月12日 下午8:01:29
+ * @version V1.0
+ */
+
+/** 
+ * @ClassName: Main
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ * @author 江洪亮
+ * @date 2017年11月12日 下午8:01:29
+ * 
+ */
+public class Main {
+
+	/** 
+	 * @Title: main
+	 * @Description: TODO(这里用一句话描述这个方法的作用)
+	 * @param @param args
+	 * @return void
+	 * @throws 
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car car=new Car();
+		car.Calculate();
+	}
+
+}
