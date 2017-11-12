8bc176b37ad2de53636f2fc746398b175941bc51
jianghongliang <876474290@qq.com>
2017-11-12 21:26:13 +0800

    create file 00

---
+++ ComputeWeight.java
@@ -0,0 +1,19 @@
+/**   
+ * @Title: ComputeWeight.java
+ * @Package 
+ * @Description: TODO(用一句话描述该文件做什么)
+ * @author 江洪亮
+ * @date 2017年11月12日 下午7:44:47
+ * @version V1.0
+ */
+
+/** 
+ * @ClassName: ComputeWeight
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ * @author 江洪亮
+ * @date 2017年11月12日 下午7:44:47
+ * 
+ */
+public interface ComputeWeight {
+	public double computeWeight();
+}
